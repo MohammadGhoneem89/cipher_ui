d5d3163c536668d858b43da5d6d9742020fe88f3
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-03-26 12:25:31 +0400

    onBoarding pagination

--- core/components/onBoarding/onBoardingProfileList.js
+++ core/components/onBoarding/onBoardingProfileList.js
@@ -17,27 +17,21 @@ class onBoardingProfileList extends React.Component {
         this.state = {
             status: [],
             searchCriteria: {},
-            currentPageNo: "",
-            totalRecords: "",
+            pageData: {
+                pageSize :10,
+                currentPageNo: "",
+                totalRecords: 0
+            },
             isLoading: true,
-            // addAction: [
-            //     {
-            //         "type": "pageAction",
-            //         "label": "ADD",
-            //         "labelName": "COM_AB_Add",
-            //         "actionType": "PORTLET_LINK",
-            //         "iconName": "fa fa-plus",
-            //         "URI":
-            //             "/onBoardingProfile/setup"
-            //     }
-            // ],
+
             gridData: []
         };
         this.getRequest = this.getRequest.bind(this);
         this.formSubmit = this.formSubmit.bind(this);
+        this.pageChanged=this.pageChanged.bind(this);
     }
 
-    getRequest(){
+    getRequest() {
         let Name = document.getElementById('name') == null ? "" : document.getElementById('name').value;
         let Status = document.getElementById('Status') == null ? "" : document.getElementById('Status').value;
         // console.log("<<<<<<<<getrequest", this.state.name, "   ", this.state.statusValue)
@@ -53,7 +47,7 @@ class onBoardingProfileList extends React.Component {
         let request = {
             "action": "getOnBoardingProfileList",
             "searchCriteria": searchCriteria,
-            "page": {
+            "pageData": {
                 "pageSize": 10,
                 "currentPageNo": this.state.currentPageNo || 1,
                 "totalRecords": this.state.totalRecords || 0
@@ -74,14 +68,14 @@ class onBoardingProfileList extends React.Component {
                 status: nextProps.typeData.onBoardingStatus,
                 gridData: nextProps.list,
                 isLoading: false,
-                currentPageNo: nextProps.pageData.currentPageNo,
-                totalRecords: nextProps.pageData.totalRecords
+                pageData: nextProps.pageData,
+                
             });
         }
     }
 
     componentDidMount() {
-        this.props.actions.generalProcess(constants.getOnBoardingList,this.getRequest());
+        this.props.actions.generalProcess(constants.getOnBoardingList, this.getRequest());
         this.props.actions.generalProcess(constants.getTypeData, requestCreator.createTypeDataRequest(['onBoardingStatus']));
         window.scrollTo(0, 0);
     }
@@ -90,13 +84,14 @@ class onBoardingProfileList extends React.Component {
         if (pageNo) {
             let pageData = this.state.pageData;
             pageData.currentPageNo = pageNo;
-            this.setState({ pageData });
-            this.getRequest({ pageNo });
+            this.setState({ pageData:pageData });
+            console.log(pageData,"<=========pageData")
+            // this.props.actions.generalProcess(constants.getOnBoardingList, this.getRequest());
         }
     }
 
     formSubmit() {
-        this.props.actions.generalProcess(constants.getOnBoardingList,this.getRequest());
+        this.props.actions.generalProcess(constants.getOnBoardingList, this.getRequest());
     }
     render() {
         if (this.state.isLoading) {
@@ -106,58 +101,57 @@ class onBoardingProfileList extends React.Component {
         return (
             <div>
                 {/* <Portlet title={utils.getLabelByID("OnBoarding Search")}> */}
-                    <div className="row">
-                        <div className="col-md-12 ">
-                            <div className="portlet light bordered sdg_portlet">
-                                <div className="portlet-title">
-                                    <div className="caption">
-                                        <span className="caption-subject">{utils.getLabelByID("OnBoarding Search")}</span></div>
-                                    <div className="tools">
-                                        <a href="javascript:;" className="collapse" data-original-title title> </a>
-                                    </div>
+                <div className="row">
+                    <div className="col-md-12 ">
+                        <div className="portlet light bordered sdg_portlet">
+                            <div className="portlet-title">
+                                <div className="caption">
+                                    <span className="caption-subject">{utils.getLabelByID("OnBoarding Search")}</span></div>
+                                <div className="tools">
+                                    <a href="javascript:;" className="collapse" data-original-title title> </a>
                                 </div>
-                                <div className="portlet-body">
-                                    <div className="form-body" id="ApiListData">
-                                        <div className="row">
-                                            <div className="col-md-12">
+                            </div>
+                            <div className="portlet-body">
+                                <div className="form-body" id="ApiListData">
+                                    <div className="row">
+                                        <div className="col-md-12">
 
-                                                <div className="row">
-                                                    <div className="col-md-6">
-                                                        <div className="form-group col-md-4">
-                                                            <label className="control-label">{utils.getLabelByID("Name")}</label>
-                                                        </div>
-                                                        <div className="form-group col-md-8">
-                                                            <input type="text" className="form-control" name="name" id="name"
-                                                            />
-                                                        </div>
+                                            <div className="row">
+                                                <div className="col-md-6">
+                                                    <div className="form-group col-md-4">
+                                                        <label className="control-label">{utils.getLabelByID("Name")}</label>
                                                     </div>
-                                                    <div className="col-md-6">
-                                                        <div className="form-group col-md-4">
-                                                            <label className="control-label">{utils.getLabelByID("Status")}</label>
-                                                        </div>
-                                                        <div className="form-group col-md-8">
-                                                            <select name="Status" id="Status" className="form-control">
-                                                                <option key="-1" value='' >{utils.getLabelByID("RA_Select")} </option>
-                                                                {this.state.status.map((option, index) => {
-                                                                    return (
-                                                                        <option key={index} value={option.value} >{option.label}</option>
-                                                                    );
-                                                                })}
-                                                            </select>
-                                                        </div>
+                                                    <div className="form-group col-md-8">
+                                                        <input type="text" className="form-control" name="name" id="name"
+                                                        />
                                                     </div>
-
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <div className="form-group col-md-4">
+                                                        <label className="control-label">{utils.getLabelByID("Status")}</label>
+                                                    </div>
+                                                    <div className="form-group col-md-8">
+                                                        <select name="Status" id="Status" className="form-control">
+                                                            <option key="-1" value='' >{utils.getLabelByID("RA_Select")} </option>
+                                                            {this.state.status.map((option, index) => {
+                                                                return (
+                                                                    <option key={index} value={option.value} >{option.label}</option>
+                                                                );
+                                                            })}
+                                                        </select>
+                                                    </div>
                                                 </div>
 
-                                                <div className="form-actions right">
-                                                    <div className="form-group col-md-12">
-                                                        <div className="btn-toolbar pull-right">
+                                            </div>
 
-                                                            <button type="submit" className="btn green" onClick={this.formSubmit} >{utils.getLabelByID("Search")} </button>
-                                                            {"  "}
-                                                            <button type="button" className="btn default" >{utils.getLabelByID("Clear")}</button>
+                                            <div className="form-actions right">
+                                                <div className="form-group col-md-12">
+                                                    <div className="btn-toolbar pull-right">
 
-                                                        </div>
+                                                        <button type="submit" className="btn green" onClick={this.formSubmit} >{utils.getLabelByID("Search")} </button>
+                                                        {"  "}
+                                                        <button type="button" className="btn default" >{utils.getLabelByID("Clear")}</button>
+
                                                     </div>
                                                 </div>
                                             </div>
@@ -167,36 +161,37 @@ class onBoardingProfileList extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <Portlet title={""} isPermissioned={true} 
+                </div>
+                <Portlet title={""} isPermissioned={true}
                     title={utils.getLabelByID("OnBoarding List")}
-                        // actions={this.state.addAction}
-                        >
-                        {
-                            this.state.gridData.map((obj) => {
-                                obj.action = [
-                                    {
-                                        "label": "View",
-                                        "URI": [
-                                            "/onBoardingProfile/setup/"
-                                        ],
-                                        "params": "_id",
-                                        "iconName": "icon-docs"
-                                    }
-                                ]
-                            })
-                        }
-                        <Table
-                            gridColumns={utils.getGridColumnByName("onBoardingProfile")}
-                            gridData={this.state.gridData}
-                            fontclass=""
-                            totalRecords={this.state.totalRecords}
-                            pageSize={10}
-                            pageChanged={this.pageChanged}
-                            pagination={true}
-                            search={true}
-                            activePage={this.state.currentPageNo}
-                        />
-                    </Portlet>
+                // actions={this.state.addAction}
+                >
+                    {
+                        this.state.gridData.map((obj) => {
+                            obj.action = [
+                                {
+                                    "label": "View",
+                                    "URI": [
+                                        "/onBoardingProfile/setup/"
+                                    ],
+                                    "params": "_id",
+                                    "iconName": "icon-docs"
+                                }
+                            ]
+                        })
+                    }
+                    <Table
+                        gridColumns={utils.getGridColumnByName("onBoardingProfile")}
+                        gridData={this.state.gridData}
+                        fontclass=""
+                        totalRecords={this.state.pageData.totalRecords}
+                        pageSize={10}
+                        pageChanged={this.pageChanged}
+                        pagination={true}
+                        search={true}
+                        activePage={this.state.pageData.currentPageNo}
+                    />
+                </Portlet>
                 {/* </Portlet> */}
             </div>
         );
