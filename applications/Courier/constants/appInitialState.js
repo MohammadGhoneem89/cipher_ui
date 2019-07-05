240d663788e25bafe85d8938d196f24ba9c3b73b
ArhamAliQureshi <arham.ali@avanzasolutions.com>
2019-07-05 22:19:58 +0400

    .

--- applications/Courier/constants/appInitialState.js
+++ applications/Courier/constants/appInitialState.js
@@ -1,31 +1,151 @@
 export default {
-    paymentList : {
-        action: "",
-        pageData: {},
-        data: {
-            searchResult: []
+  getEndToEndTrackingData: {
+    "order": {
+      "StatusList": [
+        {
+          "label": "Finalized",
+          "status": false,
+          "type": "SUCCESS",
+          "legend": "Finalized"
+        },
+        {
+          "label": "HAWB Created",
+          "status": true,
+          "type": "SUCCESS",
+          "legend": "HAWB Created"
+        },
+        {
+          "label": "Cleared",
+          "status": false,
+          "type": "SUCCESS",
+          "legend": "Cleared"
+        },
+        {
+          "label": "Delivered",
+          "status": true,
+          "type": "SUCCESS",
+          "legend": "Delivered"
         }
-    },
-    getPaymentDetail : {
-        action : "",
-        data : {}
-    },
-    getOnBoardingList:{
-        action : "",
-        pageData : {},
-        data:{
-            searchResult:[]
+      ],
+      "orderID": "",
+      "eCommerceCompanyName": "",
+      "eCommerceCompanyLogo": "",
+      "courierCompanyName": "",
+      "courierCompanyLogo": "",
+      "blockchainTranID": "",
+      "orderDate": "",
+      "soldTo": "",
+      "soldAddress": "",
+      "billTo": "",
+      "billToAddress": "",
+      "shipTo": "",
+      "shipAddress": "",
+      "totalValue": "",
+      "noOfItems": "",
+      "currency": "",
+      "incoTerms": "",
+      "returnPolicy": "",
+      "orderStatus": "",
+      "lineItems": [
+        {
+          "lineNo": "",
+          "quantity": "",
+          "description": "",
+          "hscode": "",
+          "unitPrice": "",
+          "countryOfOrigin": "",
+          "discount": "",
+          "total": "",
+          "returnReason": "",
+          "statQuantity": "",
+          "netWeight": "",
+          "statUOM": "",
+          "proofOfDeliveryHash": "",
+          "deliveryImagePath": "",
+          "deliveryToPersonName": "",
+          "isReturned": false,
+          "isDelivered": true,
+          "importDeclarationReferences": [
+            {
+              "statQuantity": "",
+              "statWeight": "",
+              "statUOM": "",
+              "newAWB": "",
+              "proofOfDeliveryHash": "",
+              "deliveryImagePath": "",
+              "deliveryToPersonName": ""
+            }
+          ]
         }
-    },
-    getOnBoardingProStructList:{
-        action : "",
-        pageData : {},
-        data:{
-            searchResult:[]
+      ],
+      "HAWB": [
+        {
+          "noOfBoxes": "",
+          "HAWBNumber": "",
+          "HAWBHashImage": "",
+          "HAWBImagePath": "",
+          "physicalWeight": "",
+          "volumetricWeight": "",
+          "isDelivered": true,
+          "proofOfDeliveryHash": "",
+          "deliveryImagePath": "",
+          "deliveryDate": "",
+          "deliveryToPersonName": "",
+          "oldHAWBNo": "",
+          "returnReason": "",
+          "isReturnHAWB": "",
+          "shippingDetails": {
+            "MAWBNumber": "",
+            "flightNo": "",
+            "portOfLoad": "",
+            "exitPort": "",
+            "cargoHandlerCode": "",
+            "brokerCode": "",
+            "brokerName": "",
+            "agentCode": "",
+            "agentName": "",
+            "flightDate": ""
+          }
         }
-    },
-    getOnBoardingProfileDetail : {
-        action : "",
-        data:{}
+      ],
+      "ExportDeclaration": [
+        {
+          "declarationNo": "",
+          "declarationID": "",
+          "version": "",
+          "exporterMirsal2Code": "",
+          "regimeType": "",
+          "declType": "",
+          "noOfPages": "",
+          "transportMode": "",
+          "invoiceNo": "",
+          "status": "",
+          "batchReqNo": "",
+          "batchStatus": "",
+          "exceptionCode": "",
+          "exceptionDetails": "",
+          "originalPayloadHash": ""
+        }
+      ],
+      "importDeclaration": [
+        {
+          "declarationNo": "",
+          "declarationID": "",
+          "version": "",
+          "importerMirsal2Code": "",
+          "regimeType": "",
+          "declType": "",
+          "noOfPages": "",
+          "transportMode": "",
+          "invoiceNo": "",
+          "status": "",
+          "batchReqNo": "",
+          "batchStatus": "",
+          "exceptionCode": "",
+          "exceptionDetails": "",
+          "originalPayloadHash": ""
+        }
+      ]
     }
+  }
 }
