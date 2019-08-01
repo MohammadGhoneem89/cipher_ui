095ace25ae094b82a4470f5c4a337d87ce0fe352
Mohammad Kamran Haider <kamran.haider@avanzainnovations.com>
2019-08-01 16:44:28 +0400

    product catalogue

--- applications/starta/appRoutes.js
+++ applications/starta/appRoutes.js
@@ -6,17 +6,17 @@ import Login from "../../core/components/AuthenticationScreens/Login.jsx";
 
 //App Routes
 import OneTimeOrder from "./components/OneTimeOrder/OneTimeOrder.jsx";
+import ProductCatalogue from "./components/ProductCatalogue/ProductCatalogue.jsx";
 
 
 let authRoutes = <div>
-  {
     <Route path="/starta/oneTimeOrder" component={OneTimeOrder} />
-  }
+    <Route path="/starta/ProductCatalogue" component={ProductCatalogue} />
 </div>;
-  
+
 let unAuthRoutes = <div>
-    <Route path="/starta/Locked" component={Locked} />
-    <Route path="/starta/login" component={Login} />
-  </div>;
-  
-export default {authRoutes, unAuthRoutes};
+  <Route path="/starta/Locked" component={Locked} />
+  <Route path="/starta/login" component={Login} />
+</div>;
+
+export default { authRoutes, unAuthRoutes };
--- applications/starta/common/Row.jsx
+++ applications/starta/common/Row.jsx
@@ -7,10 +7,10 @@ class Row extends React.Component {
 
     render() {
         return (
-            <div className="row" style={{padding:"2px"}}>
-                <div className={`col-md-${this.props.col || 12}`}>
-                    {this.props.children}
-                </div>
+            <div className="row" style={{ padding: "2px" }}>
+                {/* <div className={`col-md-${this.props.col || 12}`}> */}
+                {this.props.children}
+                {/* </div> */}
             </div >
         );
     }
