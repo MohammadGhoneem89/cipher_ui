32eeb98468f92b35380ca48ef6aa59c5c024edb0
ArhamAliQureshi <arham.ali@avanzasolutions.com>
2019-08-30 17:34:13 +0400

    .

--- applications/backOffices.js
+++ applications/backOffices.js
@@ -1,19 +1,19 @@
-//  const backOffices = {
-//     baseUrl: "http://13.80.7.122:9086",
-//     blockChainURL: "http://13.80.7.122:9086",
-//     webSocketURL: "http://13.80.7.122:9086",
-//     repostActionURL: "http://13.80.7.122:9086/API",
-//     blockChainViewerURL: "http://13.80.7.122:9086",
-//  reportUrl: "http://localhost:9087"
-//  };
  const backOffices = {
-   baseUrl: "http://localhost:9086",
-   blockChainURL: "http://localhost:9086",
-   webSocketURL: "http://localhost:9086",
-   repostActionURL: "http://localhost:9086/API",
-   blockChainViewerURL: "http://localhost:9086",
-reportUrl: "http://localhost:9087"
-};
- 
+    baseUrl: "http://13.80.7.122:9086",
+    blockChainURL: "http://13.80.7.122:9086",
+    webSocketURL: "http://13.80.7.122:9086",
+    repostActionURL: "http://13.80.7.122:9086/API",
+    blockChainViewerURL: "http://13.80.7.122:9086",
+ reportUrl: "http://localhost:9087"
+ };
+//  const backOffices = {
+//    baseUrl: "http://localhost:9086",
+//    blockChainURL: "http://localhost:9086",
+//    webSocketURL: "http://localhost:9086",
+//    repostActionURL: "http://localhost:9086/API",
+//    blockChainViewerURL: "http://localhost:9086",
+// reportUrl: "http://localhost:9087"
+// };
+//
  export default backOffices;
  
