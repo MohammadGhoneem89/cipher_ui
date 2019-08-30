7ee69be73c245fc69a08437884895033035d72d6
root <root@kubes-2.lllfyuxiwwvezi4dgmxj5xw35a.ax.internal.cloudapp.net>
2019-08-30 13:22:16 +0000

    changes

--- applications/starta/appRoutes.js
+++ applications/starta/appRoutes.js
@@ -12,13 +12,14 @@ import MasterAgreementList from "./components/MasterAgreement/MasterAgreementLis
 import ViewMasterAgreement from "./components/MasterAgreement/ViewMasterAgreement.jsx";
 import OrderList from "./components/OrderList/orderList.jsx";
 
+
 let authRoutes = <div>
     <Route path="/strata/oneTimeOrder" component={OneTimeOrder} />
     <Route path="strata/ProductCatalogue/NEWITEM" component={ProductCatalogue} />
     <Route path="/strata/ProductCatalogue/:id" component={ProductCatalogue} />
     <Route path="/strata/itemCatalogueList" component={ItemCatalogueList} />
     <Route path="/strata/MasterAgreementList" component={MasterAgreementList} />
-    <Route path="/strata/ViewMasterAgreement/:id" component={ViewMasterAgreement} />
+  <Route path="/strata/ViewMasterAgreement/:id" component={ViewMasterAgreement} />
     <Route path="/strata/orderList" component={OrderList} />
     <Route path="/strata/viewOrder/:id" component={OrderList} />
 </div>;
