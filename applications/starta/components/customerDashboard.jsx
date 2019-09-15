29f6ebe0c2a64fda9dd0ec5f8bd56cbbe1f0a2fd
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-09-15 13:07:48 +0500

    dashboard fix

--- applications/starta/components/customerDashboard.jsx
+++ applications/starta/components/customerDashboard.jsx
@@ -16,7 +16,7 @@ class Dashboard extends React.Component {
         super(props);
         const pageSize = 10;
         const currentPageNo = 1;
-        const customerID = "ETIHAD"
+        const customerID = ""
         this.supplierData = [];
         this.state = {
 
@@ -96,58 +96,8 @@ class Dashboard extends React.Component {
                 customerID: customerID
             }
         };
-        this.transformStatus = this.transformStatus.bind(this);
-        this.determineStatus = this.determineStatus.bind(this);
-    }
-    transformResponse = (getSupplierMasterList) => {
-        // console.log(getSupplierMasterList, "LISTSSSSSSSS")
-        let transformSupplierMasterList = [];
-
-        if (getSupplierMasterList) {
-            // console.log(getSupplierMasterList, "LISTSSSSSSSS")
-            getSupplierMasterList.map((item) => {
-                transformSupplierMasterList.push({
-                    "supplierName": {
-                        "name": item.supplierName,
-                        "imageURL": item.logo
-                    },
-                    "supplierCountry": item.supplierCountry,
-                    "supplierEndDate": item.supplierEndDate,
-                    "supplierID": item.supplierID,
-                    "supplierSiteName": item.supplierSiteName
-
-                });
-
-            });
-
-        }
-
-        // console.log("--------------------------------",
-        //     transformSupplierMasterList, "UPDATES!!!!!!1111")
-        // return transformSupplierMasterList.sort();
-        return _.sortBy(transformSupplierMasterList, 'supplierName.name');
     }
 
-
-    transformOrderStatus = (item) => {
-        if (item) {
-            // console.log("data-----" + JSON.stringify(item))
-            switch (item.status) {
-                case "PO": { item.stage = "Purchase Order"; break; };
-                case "QC": { item.stage = "Quality Check"; break; };
-                case "ACK": { item.stage = "Acknowledged By Supplier"; break; };
-                case "PROD": { item.stage = "Production"; break; };
-                case "SUBORDER": { item.stage = "Place Suborder"; break; };
-                case "ACK-SUBORDER": { item.stage = "Suborder Acknowledged"; break; };
-                case "RECIEVED": { item.stage = "Received By Emirates"; break; };
-                case "RECEIVED2": { item.stage = "Received By Supplier"; break; };
-                default: { item.stage = item.stage }
-            }
-        }
-        //  console.log("-->>>>>>>>>>>>>>>>>>----",
-        //  data, "<-----------transformOrderStatus!!!!!!1111")
-        return item.stage;
-    }
 
     componentWillMount() {
         this.getDashboardData("");
@@ -171,23 +121,19 @@ class Dashboard extends React.Component {
         if (nextProps.data.dashboardPendingGridData) {
             console.log(nextProps.data.dashboardPendingGridData, "&&&&&&&&&&&&&&&&&")
             this.setState({
-                getSuppliersList: this.transformResponse(nextProps.suppliers),
-                getPendingOrders: this.transformStatus(nextProps.data.dashboardPendingGridData.pendingOrderRows),
-                getCompletedOrders: this.transformStatus(nextProps.data.dashboardCompletedGridData.completedOrderRows),
+                getSuppliersList: nextProps.suppliers,
+                getPendingOrders: nextProps.data.dashboardPendingGridData.pendingOrderRows,
+                getCompletedOrders: nextProps.data.dashboardCompletedGridData.completedOrderRows,
                 setPagingForSupplier: nextProps.supplierPageDate,
                 isLoading: false,
             });
-            this.supplierData = this.transformResponse(nextProps.suppliers)
         }
-        console.log(this.supplierData, "-------------------SUPPLIER DATA ")
     }
 
 
 
     getDashboardData = (supplier) => {
-        console.log(supplier, "suppleir")
         this.props.actions.generalProcess(url.customerDashboard, {
-            supplier: supplier || this.state.supplierAddress,
             // user: this.state.user,
             dashboardPendingGridData: this.state.dashboardPendingGridData,
             dashboardCompletedGridData: this.state.dashboardCompletedGridData,
@@ -209,223 +155,6 @@ class Dashboard extends React.Component {
         // this.props.actions.generalProcess(url.cusDashboardData, {pageName:this.state[pageName]})
         this.getDashboardData("ETIHAD");
     };
-    supplierChange = (e) => {
-        let suppData = this.supplierData.sort();
-
-        for (let i in suppData) {
-            if (e.target.value == suppData[i].supplierName.name) {
-                //  console.log(e.target.value+ "  " +suppData[i].supplierID, " ||||||||||||||||||  traget value ")
-                this.onSupplierChange(suppData[i].supplierID)
-            }
-            else if (e.target.value == 'ALL') {
-                // console.log(e.target.value, " ||||||||||||||||||  ALL traget value ")
-                this.onSupplierChange(e.target.value);
-            }
-
-        }
-        this.setState({ value: e.target.value });
-        // this.onSupplierChange('8314891')
-    };
-    onSupplierChange = (currentSupplier) => {
-        // alert("onsupplierchange")
-        this.setState({ isLoading: true });
-        this.state.dashboardPendingGridData.supplierID = currentSupplier;
-        this.state.dashboardCompletedGridData.supplierID = currentSupplier;
-        this.state.dashboardSettlementGridData.supplierID = currentSupplier;
-        this.state.dashboardCustomerSettlement.supplierID = currentSupplier;
-        this.getDashboardData("ETIHAD");
-        console.log(">>>>>>>>", this.state.dashboardPendingGridData.supplierID, "UPDATE ID")
-    };
-    getLoad = () => {
-        this.setState({ isLoading: true });
-        this.getDashboardData("ETIHAD");
-    }
-    getSuppliersList = () => {
-        this.props.actions.generalProcess(url.getSupplierMasterList,
-            this.state.suppliersMasterList);
-    };
-
-    transformStatus = (gridData) => {
-
-        let transformOrderList = [];
-        if (gridData) {
-            //  console.log("gridData   ", gridData)
-            gridData.map((order) => {
-                // console.log("order   ", order)
-                transformOrderList.push({
-                    "orderID": order.orderID,
-                    "customerID": order.customerID,
-                    "amount": order.amount,
-                    "dateCreated": order.dateCreated,
-                    "expectedDate": order.expectedDate,
-                    "orderType": order.orderType,
-                    // "stage": this.transformOrderStatus(order),
-                    "status": order.status,
-                    // "status": this.determineStatus(order),
-                    "actions": order.actions,
-
-                });
-            });
-        }
-
-        //console.log("transform   ", transformOrderList)
-        return transformOrderList;
-    }
-
-    determineStatus = (order) => {
-        //alert(JSON.stringify(order))
-        let totalSLATime = 0
-        for (let i = 0; i < order.sla.length; i++) {
-            //   console.log("order.sla" + order.sla[i])
-            totalSLATime += order.sla[i].duration / 1000
-        }
-        // console.log("total sla " + totalSLATime)
-
-        let actualTime = Math.round((new Date()).getTime() / 1000) - order.dateCreated
-        //let actualTime = 1555308812 - order.dateCreated
-        //console.log("actualTime: " + actualTime)
-
-        let SLAToCurrent = 0
-
-        if (order.status == "PO") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "ACK") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            // console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        if (order.status == "ACK") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "ACK") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            // console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        if (order.status == "SUBORDER") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "ACK" || order.sla[i].fromStage == "ACK" && order.sla[i].toStage == "SUBORDER") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            //console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        if (order.status == "ACK-SUBORDER") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "ACK" || order.sla[i].fromStage == "ACK" && order.sla[i].toStage == "SUBORDER" || order.sla[i].fromStage == "SUBORDER" && order.sla[i].toStage == "ACK-SUBORDER") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            // console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        if (order.status == "PROD") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "ACK" || order.sla[i].fromStage == "ACK" && order.sla[i].toStage == "SUBORDER" || order.sla[i].fromStage == "SUBORDER" && order.sla[i].toStage == "ACK-SUBORDER" || order.sla[i].fromStage == "ACK-SUBORDER" && order.sla[i].toStage == "PROD") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            // console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        else if (order.status == "QC") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "PROD" || order.sla[i].fromStage == "PROD" && order.sla[i].toStage == "QC") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            //  console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        else if (order.status == "SHIPPED") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "PROD" || order.sla[i].fromStage == "PROD" && order.sla[i].toStage == "QC" || order.sla[i].fromStage == "QC" && order.sla[i].toStage == "SHIPPED") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            //  console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        else if (order.status == "RECEIVED1") {
-            for (let i = 0; i < order.sla.length; i++) {
-                if (order.sla[i].fromStage == "PO" && order.sla[i].toStage == "PROD" || order.sla[i].fromStage == "PROD" && order.sla[i].toStage == "QC" || order.sla[i].fromStage == "QC" && order.sla[i].toStage == "SHIPPED" || order.sla[i].fromStage == "SHIPPED" && order.sla[i].toStage == "RECEIVED1") {
-                    SLAToCurrent += order.sla[i].duration / 1000
-                }
-            }
-            // console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        else if (order.status == "INVOICED" || order.status == "RECEIVED2") {
-            SLAToCurrent = totalSLATime
-            actualTime = order.invoiceTime - order.dateCreated
-            // console.log("SLAToCurrent: " + SLAToCurrent)
-        }
-
-        if (actualTime >= totalSLATime) {
-            // console.log(actualTime)
-            //console.log(SLAToCurrent)
-
-            let seconds = actualTime - SLAToCurrent
-
-            let d = Math.floor(seconds / (3600 * 24));
-            let h = Math.floor(seconds % (3600 * 24) / 3600);
-            let m = Math.floor(seconds % 3600 / 60);
-            let s = Math.floor(seconds % 60);
-
-            let dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
-            let hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
-            let mDisplay = ""
-            let sDisplay = ""
-            if (d <= 0) {
-                mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
-                if (h <= 0) {
-                    sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-                }
-            }
-
-
-            return {
-                value: "Delayed By " + dDisplay + hDisplay + mDisplay + sDisplay,
-                type: "ERROR"
-            }
-        }
-        else if (actualTime < totalSLATime) {
-            if (actualTime - SLAToCurrent <= 0) {
-                return {
-                    value: "On Time",
-                    type: "SUCCESS"
-                }
-            }
-            else {
-                let seconds = actualTime - SLAToCurrent
-                let d = Math.floor(seconds / (3600 * 24));
-                let h = Math.floor(seconds % (3600 * 24) / 3600);
-                let m = Math.floor(seconds % 3600 / 60);
-                let s = Math.floor(seconds % 60);
-
-                let dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
-                let hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
-                let mDisplay = ""
-                let sDisplay = ""
-                if (d <= 0) {
-                    mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
-                    if (h <= 0) {
-                        sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-                    }
-                }
-
-
-                return {
-                    value: "Expected Delay By " + dDisplay + hDisplay + mDisplay + sDisplay,
-                    type: "WARNING"
-                }
-            }
-        }
-    }
 
     render() {
         
