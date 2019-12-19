4c2697cc3808c537a992f3b9adab1740a11ff348
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-12-19 10:12:12 +0500

    backoffice

--- applications/backOffices.js
+++ applications/backOffices.js
@@ -1,19 +1,18 @@
-//  const backOffices = {
-//     baseUrl: "http://13.80.7.122:9086",
-//     blockChainURL: "http://13.80.7.122:9086",
-//     webSocketURL: "http://13.80.7.122:9086",
-//     repostActionURL: "http://13.80.7.122:9086/API",
-//     blockChainViewerURL: "http://13.80.7.122:9086",
-//  reportUrl: "http://localhost:9087"
-//  };
  const backOffices = {
-   baseUrl: "http://localhost:9086",
-   blockChainURL: "http://localhost:9086",
-   webSocketURL: "http://localhost:9086",
-   repostActionURL: "http://localhost:9086/API",
-   blockChainViewerURL: "http://localhost:9086",
-reportUrl: "http://localhost:9087"
-};
+     baseUrl: "http://40.123.215.26:9080",
+     blockChainURL: "http://40.123.215.26:9080",
+     webSocketURL: "http://40.123.215.26:9080",
+     repostActionURL: "http://40.123.215.26:9080/API",
+     blockChainViewerURL: "http://40.123.215.26:9080",
+     IPFS: "//40.123.215.26:9080/API/core/download?type=IPFS&path="
+ };
+ //  const backOffices = {
+ //    baseUrl: "http://localhost:9086",
+ //    blockChainURL: "http://localhost:9086",
+ //    webSocketURL: "http://localhost:9086",
+ //    repostActionURL: "http://localhost:9086/API",
+ //    blockChainViewerURL: "http://localhost:9086",
+ // reportUrl: "http://localhost:9087"
+ // };
 
  export default backOffices;
- 
