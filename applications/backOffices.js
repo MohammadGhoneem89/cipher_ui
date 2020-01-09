373c7520934f9ae59d0d0095206aecb3834a3715
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2020-01-09 10:56:49 +0400

    localhost route

--- applications/backOffices.js
+++ applications/backOffices.js
@@ -17,7 +17,7 @@
 // };
 const backOffices = {
     baseUrl: "http://localhost:9086",
-    blockChainURL: "http://localhost:9086",
+    blockChainURL: "http://localhost:9086",         
     webSocketURL: "http://localhost:9086",
     repostActionURL: "http://localhost:9086/API",
     blockChainViewerURL: "http://localhost:9086",
