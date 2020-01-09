71c98139edc80c27bde962138fb59710b9b08291
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2020-01-09 10:56:17 +0400

    localhost route

--- applications/backOffices.js
+++ applications/backOffices.js
@@ -23,5 +23,14 @@ const backOffices = {
     blockChainViewerURL: "http://localhost:9086",
     reportUrl: "http://localhost:9087"
 };
+// const backOffices={
+    
+//     baseUrl: "http://13.80.7.122:9086",
+//     blockChainURL: "http://13.80.7.122:9086",
+//     webSocketURL: "http://13.80.7.122:9086",
+//     repostActionURL: "http://13.80.7.122:9086/API",
+//     blockChainViewerURL: "http://13.80.7.122:9086",
+//     reportUrl: "http://13.80.7.122:9087"
+// }
 
 export default backOffices;
