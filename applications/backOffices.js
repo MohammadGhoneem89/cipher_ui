62934f0639bf5e8df1ec1de8c18976aa61e70188
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2020-01-13 09:55:00 +0400

    changes for smiles

--- applications/backOffices.js
+++ applications/backOffices.js
@@ -1,20 +1,4 @@
-//  const backOffices = {
-//      baseUrl: "http://40.123.204.129:9080",
-//      blockChainURL: "http://40.123.204.129:9080",
-//      webSocketURL: "http://40.123.204.129:9080",
-//      repostActionURL: "http://40.123.204.129:9080/API",
-//      blockChainViewerURL: "http://40.123.204.129:9080",
-//      IPFS: "http://40.123.204.129:9080/API/core/download?type=IPFS&path="
-//  };
 
-// const backOffices = {
-//     baseUrl: "http://40.123.215.26:9080",
-//     blockChainURL: "http://40.123.215.26:9080",
-//     webSocketURL: "http://40.123.215.26:9080",
-//     repostActionURL: "http://40.123.215.26:9080/API",
-//     blockChainViewerURL: "http://40.123.215.26:9080",
-//     IPFS: "http://40.123.215.26:9080/API/core/download?type=IPFS&path="
-// };
 const backOffices = {
     baseUrl: "http://localhost:9086",
     blockChainURL: "http://localhost:9086",         
