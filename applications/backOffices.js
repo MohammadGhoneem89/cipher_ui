174e8150cdb5ae7fbd4cc328d1d8ef6f54b88ec9
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2020-01-19 17:50:47 +0400

    baseurl changed

--- applications/backOffices.js
+++ applications/backOffices.js
@@ -1,20 +1,19 @@
 
-const backOffices = {
-    baseUrl: "http://localhost:9086",
-    blockChainURL: "http://localhost:9086",         
-    webSocketURL: "http://localhost:9086",
-    repostActionURL: "http://localhost:9086/API",
-    blockChainViewerURL: "http://localhost:9086",
-    reportUrl: "http://localhost:9087"
-};
-// const backOffices={
-    
-//     baseUrl: "http://13.80.7.122:9086",
-//     blockChainURL: "http://13.80.7.122:9086",
-//     webSocketURL: "http://13.80.7.122:9086",
-//     repostActionURL: "http://13.80.7.122:9086/API",
-//     blockChainViewerURL: "http://13.80.7.122:9086",
-//     reportUrl: "http://13.80.7.122:9087"
-// }
+// const backOffices = {
+//     baseUrl: "http://localhost:9081",
+//     blockChainURL: "http://localhost:9081",         
+//     webSocketURL: "http://localhost:9081",
+//     repostActionURL: "http://localhost:9081/API",
+//     blockChainViewerURL: "http://localhost:9081",
+//     reportUrl: "http://localhost:9087"
+// };
+const backOffices={
+    baseUrl: "http://23.97.138.116:9081",
+    blockChainURL: "http://23.97.138.116:9081",
+    webSocketURL: "http://23.97.138.116:9081",
+    repostActionURL: "http://23.97.138.116:9081/API",
+    blockChainViewerURL: "http://23.97.138.116:9081",
+    reportUrl: "http://23.97.138.116:9087"
+}
 
 export default backOffices;
