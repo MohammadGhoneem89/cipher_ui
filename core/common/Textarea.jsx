1a257cb81afa41d849f47c9535de9ca43317f63a
Aiman Saeed <aiman.saeed@avanzainnovations.com>
2020-01-28 18:22:51 +0400

    add on

--- core/common/Textarea.jsx
+++ core/common/Textarea.jsx
@@ -7,6 +7,7 @@ class Input extends React.Component {
                 <textarea style={{ ...this.props.style }} id={this.props.fieldname} name={this.props.fieldname} rows={this.props.rows}
                     cols={this.props.cols}
                     className={'form-control'}
+                    placeholder={_.get(this.props, "placeholder", "")}
                     value={this.props.value ||
                         _.get(this.props.state, `${this.props.formname}.${[this.props.fieldname]}`, "")}
                     disabled={this.props.disabled}
