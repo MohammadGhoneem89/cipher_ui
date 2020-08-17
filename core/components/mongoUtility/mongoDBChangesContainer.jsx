dceaef1e0e8887b76e295ad8d66650a35b8853bd
Isma Noor <dev@avanzainnovations.com>
2020-08-18 03:56:22 +0500

    mongoDbUtility: - added two seperate functionalities for new & updated doc - added prev and next button with functionality for new & updated documents

--- core/components/mongoUtility/mongoDBChangesContainer.jsx
+++ core/components/mongoUtility/mongoDBChangesContainer.jsx
@@ -60,9 +60,14 @@ class MongoDBChangesContainer extends React.Component {
         this.ActionHandlers = this.ActionHandlers.bind(this);
         this.submit = this.submit.bind(this);
         this.showSingleSchemaChange = this.showSingleSchemaChange.bind(this);
-        this.closeCollectionDiffModal = this.closeCollectionDiffModal.bind(this);
+        this.closeNewDocumentsModal = this.closeNewDocumentsModal.bind(this);
+        this.closeUpdatedDocumentsModal = this.closeUpdatedDocumentsModal.bind(this);
         this.closeMigrationAlertModal = this.closeMigrationAlertModal.bind(this);
         this.updateState = this.updateState.bind(this);
+        this.loadPrevious_NewDocuments = this.loadPrevious_NewDocuments.bind(this);
+        this.loadNext_NewDocuments = this.loadNext_NewDocuments.bind(this);
+        this.loadPrevious_UpdatedDocuments = this.loadPrevious_UpdatedDocuments.bind(this);
+        this.loadNext_UpdatedDocuments = this.loadNext_UpdatedDocuments.bind(this);
 
     }
 
@@ -72,21 +77,36 @@ class MongoDBChangesContainer extends React.Component {
     }
 
     setCurrentCollectionName(index) {
-        this.setState({
-            currentModelName: this.state.mongodbSchemaChanges[index].modelName
-        })
+        if (this.state.mongodbSchemaChanges[index].type == "new") {
+            this.setState({
+                currentModelName: this.state.mongodbSchemaChanges[index].modelName,
+            })
+        } else {
+            if (this.state.mongodbSchemaChanges[index].new_documents.count > 0) {
+                this.setState({
+                    currentNewDocIndex: this.state.mongodbSchemaChanges[index].new_documents.data[0],
+                })
+            }
+            if (this.state.mongodbSchemaChanges[index].updated_documents.count > 0) {
+                this.setState({
+                    currentUpdatedDocIndex: this.state.mongodbSchemaChanges[index].updated_documents.data[0],
+                })
+            }
+        }
     }
 
     ActionHandlers({ actionName, index }) {
 
         this.setCurrentCollectionName(index);
         switch (actionName) {
-            case "viewCollectionChanges":
-                this.showSingleSchemaChange("Group", "new", 0);
-                this.setState({ isOpenCollectionDiffModal: true, currentIndex: index })
+            case "view New Documents":
+                this.loadDefaultDocument(index, "new");
+                break;
+            case "view Updated Documents":
+                this.loadDefaultDocument(index, "updated");
                 break;
             case "migrateCollection":
-                this.setState({ isOpenMigrationAlert: true, currentIndex: index })
+                this.setState({ isOpenMigrationAlert: true, currentModelIndex: index })
                 break;
             default:
                 break;
@@ -119,7 +139,8 @@ class MongoDBChangesContainer extends React.Component {
                     ]
                 } else {
                     elem.actions = [
-                        { "label": "viewCollectionChanges", "iconName": "fa fa-pen", "actionType": "COMPONENT_FUNCTION" }
+                        { "label": "view New Documents", "iconName": "fa fa-pen", "actionType": "COMPONENT_FUNCTION" },
+                        { "label": "view Updated Documents", "iconName": "fa fa-pen", "actionType": "COMPONENT_FUNCTION" }
                     ]
                 }
 
@@ -136,7 +157,7 @@ class MongoDBChangesContainer extends React.Component {
         }
         if (nextProps.upsertMongodbChange) {
             let array = [...this.state.mongodbSchemaChanges];
-            array.splice(this.state.currentIndex, 1);
+            array.splice(this.state.currentModelIndex, 1);
             this.setState({
                 isMigrating: false,
                 mongodbSchemaChanges: array
@@ -165,11 +186,109 @@ class MongoDBChangesContainer extends React.Component {
         }));
     }
 
-    closeCollectionDiffModal() {
-        this.setState({ isOpenCollectionDiffModal: false })
+    closeNewDocumentsModal() {
+        this.setState({ isOpenNewDocumentsModal: false })
+    }
+    closeUpdatedDocumentsModal() {
+        this.setState({ isOpenUpdatedDocumentsModal: false })
     }
     closeMigrationAlertModal() {
         this.setState({ isOpenMigrationAlert: false })
+    }
+
+    loadDefaultDocument(index, type) {
+
+        if (type == "new") {
+            this.setState({
+                currentNewDocIndex: 0,
+                currentNewDoc: this.state.mongodbSchemaChanges[index].new_documents.data[0],
+                isOpenNewDocumentsModal: true,
+                currentModelIndex: index
+            })
+        } else {
+            this.setState({
+                currentUpdatedDocIndex: 0,
+                currentUpdatedDoc: this.state.mongodbSchemaChanges[index].updated_documents.data[0],
+                isOpenUpdatedDocumentsModal: true,
+                currentModelIndex: index
+            })
+        }
+
+    }
+
+    loadPrevious_NewDocuments() {
+        //this.setState({ isOpenMigrationAlert: false })
+        console.log(this.state.currentNewDocIndex)
+        let current = this.state.currentNewDocIndex,
+            newDocArrSize = this.state.mongodbSchemaChanges[this.state.currentModelIndex].new_documents.data.length;
+
+        if (current === 0) {
+            let index = newDocArrSize - 1,
+                new_document = this.state.mongodbSchemaChanges[this.state.currentModelIndex].new_documents.data[index];
+            this.setState({
+                currentNewDocIndex: index,
+                currentNewDoc: new_document
+            })
+        } else {
+            let index = current - 1,
+                new_document = this.state.mongodbSchemaChanges[this.state.currentModelIndex].new_documents.data[index];
+            this.setState({
+                currentNewDocIndex: index,
+                currentNewDoc: new_document
+            })
+        }
+    }
+
+    loadNext_NewDocuments() {
+        //this.setState({ isOpenMigrationAlert: false })
+        console.log(this.state.currentNewDocIndex)
+
+        let current = this.state.currentNewDocIndex + 1,
+            newDocArrSize = this.state.mongodbSchemaChanges[this.state.currentModelIndex].new_documents.data.length,
+            next = current % newDocArrSize,
+            new_document = this.state.mongodbSchemaChanges[this.state.currentModelIndex].new_documents.data[next];
+
+        this.setState({
+            currentNewDocIndex: next,
+            currentNewDoc: new_document
+        })
+    }
+
+    loadPrevious_UpdatedDocuments() {
+        //this.setState({ isOpenMigrationAlert: false })
+        console.log(this.state.currentUpdatedDocIndex)
+        let current = this.state.currentUpdatedDocIndex,
+            newDocArrSize = this.state.mongodbSchemaChanges[this.state.currentModelIndex].updated_documents.data.length;
+
+        if (current === 0) {
+            let index = newDocArrSize - 1,
+                document = this.state.mongodbSchemaChanges[this.state.currentModelIndex].updated_documents.data[index];
+            this.setState({
+                currentUpdatedDocIndex: index,
+                currentUpdatedDoc: document
+            })
+        } else {
+            let index = current - 1,
+                document = this.state.mongodbSchemaChanges[this.state.currentModelIndex].updated_documents.data[index];
+            this.setState({
+                currentUpdatedDoc: index,
+                currentUpdatedDoc: document
+            })
+        }
+    }
+
+    loadNext_UpdatedDocuments() {
+        //this.setState({ isOpenMigrationAlert: false })
+        console.log(this.state.currentUpdatedDocIndex)
+        let current = this.state.currentUpdatedDocIndex + 1,
+            newDocArrSize = this.state.mongodbSchemaChanges[this.state.currentModelIndex].updated_documents.data.length,
+            next = current % newDocArrSize,
+            document = this.state.mongodbSchemaChanges[this.state.currentModelIndex].updated_documents.data[next];
+
+        this.setState({
+            currentUpdatedDocIndex: next,
+            currentUpdatedDoc: document
+        })
     }
 
     migrateDBChange(type) {
@@ -263,8 +382,22 @@ class MongoDBChangesContainer extends React.Component {
                             </div>
                         </Portlet>
                     </ModalBox>
-                    <ModalBox isOpen={this.state.isOpenCollectionDiffModal}>
+                    <ModalBox isOpen={this.state.isOpenNewDocumentsModal}>
                         <Portlet title={"Collection Difference alert"} isPermissioned={true}>
+                            <div className="row" >
+                                <div className="form-actions right">
+                                    <div className="form-group col-md-6">
+                                        <div className="btn-toolbar pull-left">
+                                            <button type="button" className="btn btn-default" onClick={this.loadPrevious_NewDocuments} >{"Prev"}</button>
+                                        </div>
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <div className="btn-toolbar pull-right">
+                                            <button type="button" className="btn btn-default" onClick={this.loadNext_NewDocuments} >{"Next"}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <div className="row" >
                                 <div className="form-group col-md-12">
                                     <h3 className="modal-title">{"Do you want to migrate "}{this.state.documentType} {" document?"}</h3>
@@ -277,18 +410,11 @@ class MongoDBChangesContainer extends React.Component {
                                 <div className="form-group col-md-9">
                                     <label className="control-label">{this.state.currentModelName}</label>
                                 </div>
-                                <div className="form-group col-md-3">
-                                    <label className="control-label bold">{"Type of Change: "}</label>
-                                </div>
-                                <div className="form-group col-md-9">
-                                    <label
-                                        className="control-label ">{this.state.documentType}</label>
-                                </div>
                             </div>
                             <div className="row" >
                                 <div className="col-md-12">
                                     <div className="form-group col-md-12">
-                                        {this.state.documentType == "new" ? <newDocumentsModalLayout /> : <updatedDocumentsModalLayout />}
+                                        <pre> <ReactJson src={this.state.currentNewDoc} /></pre>
                                     </div>
                                 </div>
                             </div>
@@ -296,7 +422,54 @@ class MongoDBChangesContainer extends React.Component {
                                 <div className="form-actions right">
                                     <div className="form-group col-md-12">
                                         <div className="btn-toolbar pull-right">
-                                            <button type="button" className="btn btn-default" onClick={this.closeCollectionDiffModal} disabled={this.state.isMigrating}>{utils.getLabelByID("Close")}</button>
+                                            <button type="button" className="btn btn-default" onClick={this.closeNewDocumentsModal} disabled={this.state.isMigrating}>{utils.getLabelByID("Close")}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Portlet>
+                    </ModalBox>
+                    <ModalBox isOpen={this.state.isOpenUpdatedDocumentsModal}>
+                        <Portlet title={"Collection Difference alert"} isPermissioned={true}>
+                            <div className="row" >
+                                <div className="form-actions right">
+                                    <div className="form-group col-md-6">
+                                        <div className="btn-toolbar pull-left">
+                                            <button type="button" className="btn btn-default" onClick={this.loadPrevious_UpdatedDocuments} >{"Prev"}</button>
+                                        </div>
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        <div className="btn-toolbar pull-right">
+                                            <button type="button" className="btn btn-default" onClick={this.loadNext_UpdatedDocuments} >{"Next"}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row" >
+                                <div className="form-group col-md-12">
+                                    <h3 className="modal-title">{"Do you want to migrate "}{this.state.documentType} {" document?"}</h3>
+                                </div>
+                            </div>
+                            <div className="row" >
+                                <div className="form-group col-md-3">
+                                    <label className="control-label bold">{"Model Name:"}</label>
+                                </div>
+                                <div className="form-group col-md-9">
+                                    <label className="control-label">{this.state.currentModelName}</label>
+                                </div>
+                            </div>
+                            <div className="row" >
+                                <div className="col-md-12">
+                                    <div className="form-group col-md-12">
+                                        <pre> <ReactJson src={this.state.currentUpdatedDoc} /></pre>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row" >
+                                <div className="form-actions right">
+                                    <div className="form-group col-md-12">
+                                        <div className="btn-toolbar pull-right">
+                                            <button type="button" className="btn btn-default" onClick={this.closeUpdatedDocumentsModal} disabled={this.state.isMigrating}>{utils.getLabelByID("Close")}</button>
                                         </div>
                                     </div>
                                 </div>
